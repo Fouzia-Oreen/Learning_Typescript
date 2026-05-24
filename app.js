c5f5341c7f4f534deca3b0b0e090f9b44fea9f58
Fouzia Oreen <fouziaoreen@gmail.com>
2026-05-24 13:07:49 +0600

    Declaration Variable

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-var userName = "John Doe";
-console.log("Hello, ".concat(userName, "! Welcome to TypeScript 6.0.3 with ESNext modules and ES2024 target."));
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userName = void 0;
+exports.userName = "John Doe";
+console.log("Hello, ".concat(exports.userName, "! Welcome to TypeScript 6.0.3 with ESNext modules and ES2024 target."));
