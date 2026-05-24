78f661ebb361dad0fc516eeb1e25f7ef73bbb275
Fouzia Oreen <fouziaoreen@gmail.com>
2026-05-24 13:04:37 +0600

    learning-typescript

---
+++ app.js
@@ -0,0 +1,2 @@
+var userName = "John Doe";
+console.log("Hello, ".concat(userName, "! Welcome to TypeScript 6.0.3 with ESNext modules and ES2024 target."));
---
+++ app.ts
@@ -0,0 +1,2 @@
+let userName : string = "John Doe";
+console.log(`Hello, ${userName}! Welcome to TypeScript 6.0.3 with ESNext modules and ES2024 target.`); 
