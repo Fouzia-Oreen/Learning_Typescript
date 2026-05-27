a9eacaae62ee18e3e50adf1714ecd5a769b8f629
Fouzia Oreen <fouziaoreen@gmail.com>
2026-05-27 11:30:10 +0600

    all the variables

---
+++ src/variables.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.anyBoolean = exports.anyString = exports.anyNumber = void 0;
+var anyNumber = 5;
+exports.anyNumber = anyNumber;
+var anyString = "Hello World";
+exports.anyString = anyString;
+var anyBoolean = true;
+exports.anyBoolean = anyBoolean;
---
+++ src/variables.ts
@@ -0,0 +1,5 @@
+let anyNumber : Number = 5;
+let anyString : String = "Hello World";
+let anyBoolean : Boolean = true;
+
+export { anyNumber, anyString, anyBoolean };
